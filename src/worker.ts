bd1a6c529e5018b9ad1dc922a1ea8d52de1f9be3
alex-vukov <alexvukov@gmail.com>
2023-05-31 18:06:51 +0300

    Adjust worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,20 @@
 import Piscina from "piscina";
 import path from "path";
 
-const piscina = new Piscina({ filename: path.resolve(__dirname, "do_work.js") });
+type CreateOptions = Omit<ConstructorParameters<typeof Piscina>[0], "filename">;
 
-export const codec = () => ({
-  compress: async (encoder: { buffer: Buffer }) => {
-    const compressed = await piscina.run({ buffer: encoder.buffer }, { name: "compress" });
-    return Buffer.from(compressed);
-  },
+export const createCodec = (options: CreateOptions) => {
+  const piscina = new Piscina({ ...options, filename: path.resolve(__dirname, "do_work.js") });
 
-  decompress: async (buffer: Buffer) => {
-    const decompressed = await piscina.run({ buffer }, { name: "decompress" });
-    return Buffer.from(decompressed);
-  },
-});
+  return () => ({
+    compress: async (encoder: { buffer: Buffer }) => {
+      const compressed = await piscina.run({ buffer: encoder.buffer }, { name: "compress" });
+      return Buffer.from(compressed);
+    },
+
+    decompress: async (buffer: Buffer) => {
+      const decompressed = await piscina.run({ buffer }, { name: "decompress" });
+      return Buffer.from(decompressed);
+    },
+  });
+};
